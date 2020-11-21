d2bcab374d2eb75684d94e745e1c5e9aba558ff0
KlaudiaK1998 <klaudiaklepacka1998@gmail.com>
2020-11-21 12:04:55 +0100

    Add data binding

---
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-button-unit',
+  templateUrl: './input-button-unit.component.html',
+  styleUrls: ['./input-button-unit.component.scss']
+})
+export class InputButtonUnitComponent implements OnInit {
+  title = 'Hello World';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  changeTitle(newTitle: string): void {
+    this.title = newTitle;
+  }
+}
